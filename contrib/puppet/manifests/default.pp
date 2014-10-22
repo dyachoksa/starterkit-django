4b8eba8af95ab17d0ce6bc567483a08c6a92072f
Sergey Dyachok <sergey@sdyachok.com.ua>
2014-10-22 17:09:51 +0300

    Initial commit

---
+++ contrib/puppet/manifests/default.pp
@@ -0,0 +1,18 @@
+# Default dev manifest
+
+$PROJECT_DIR = "/vagrant"
+
+import "classes/*.pp"
+
+Exec {
+    path => "/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin",
+}
+
+class dev {
+    class {
+        init:;
+        database:;
+    }
+}
+
+include dev
